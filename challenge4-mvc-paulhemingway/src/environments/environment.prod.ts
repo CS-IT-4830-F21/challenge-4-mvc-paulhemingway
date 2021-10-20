ab61373a64afc8eb0abe1dd6b193b788dda50ae2
paulhemingway <phemingway@ymail.com>
2021-10-19 21:33:21 -0500

    fixed it so it's deployable

--- challenge4-mvc-paulhemingway/src/environments/environment.prod.ts
+++ challenge4-mvc-paulhemingway/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCAktY_bVUXyOaYWFp0FgeY1qlXFx8_Tfw",
+    authDomain: "challenge-4-mvc-paulhemingway.firebaseapp.com",
+    projectId: "challenge-4-mvc-paulhemingway",
+    storageBucket: "challenge-4-mvc-paulhemingway.appspot.com",
+    messagingSenderId: "444942098083",
+    appId: "1:444942098083:web:d2f8eeb675a0143cd36916",
+    measurementId: "G-VP8PJVJZZ2"
+  }
 };
