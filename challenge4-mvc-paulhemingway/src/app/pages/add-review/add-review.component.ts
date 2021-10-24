1a309c86b0af8667d280a6aa5cddacd02cb910c8
paulhemingway <phemingway@ymail.com>
2021-10-24 15:06:56 -0500

    Styling the form mostly

--- challenge4-mvc-paulhemingway/src/app/pages/add-review/add-review.component.ts
+++ challenge4-mvc-paulhemingway/src/app/pages/add-review/add-review.component.ts
@@ -9,9 +9,12 @@ import { FormsModule } from '@angular/forms';
 export class AddReviewComponent implements OnInit {
 
   stars!: String;
+  defaultStars!: String;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.stars = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>'
   }
 
   OnChange(value: String) {
@@ -19,11 +22,11 @@ export class AddReviewComponent implements OnInit {
    const number = Number(value)
 
    for (let i = 0; i < number; i++){
-    this.stars = this.stars + '<i class="bi bi-star-fill"></i>'
+    this.stars = this.stars + '<i class="fas fa-star"></i>'
    }
 
    for (let i = 0; i < 5-number; i++){
-    this.stars = this.stars + '<i class="bi bi-star"></i>'
+    this.stars = this.stars + '<i class="far fa-star"></i>'
    }
   }
   
